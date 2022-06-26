4717d0ed85c31d85a27d96519a9773732ec0493d
Murat Kahraman <muradkahraman@gmail.com>
2022-06-26 04:11:57 -0400

    updated

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         prevMap = {}
         for i, n in enumerate(nums):
             diff = target - n
