41893842daf6090fbe4addd5f009168eac407fd8
Murat Kahraman <muradkahraman@gmail.com>
2022-06-27 19:43:43 -0400

    first commit

---
+++ 15-3Sum.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i, a in enumerate(nums):
+            if a == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[r] and l < r:
+                        l += 1
+        return res
+
+
+nums = [-1, 0, 1, 2, -1, -4]
+print(Solution.threeSum(self=nums, nums=nums))
