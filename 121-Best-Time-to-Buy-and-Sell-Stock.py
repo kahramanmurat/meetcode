9d1118e07ad088b995ecb96534cd775cf5d562c4
Murat Kahraman <muradkahraman@gmail.com>
2022-06-27 21:42:42 -0400

    first commit

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
+
+
+prices = [7, 1, 5, 3, 6, 4]
+print(Solution.maxProfit(self=prices, prices=prices))
